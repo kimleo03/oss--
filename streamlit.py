65553a4746182b2a5982d18e0f65a20e0c66c853
kimleo03 <kimleo03@naver.com>
2024-03-27 09:53:32 +0900

    first commit

---
+++ streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+st.number_input('input number')
+st.title("practice")
+st.text_input('email')
+st.date_input('date')
+st.time_input('time(KOR)')
+st.text_area('description')
+st.file_uploader('upload photo')
+st.color_picker('Choose color')
