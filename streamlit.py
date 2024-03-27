70eccbc2865478bc3b32c4107a2f319469244870
kimleo03 <kimleo03@naver.com>
2024-03-27 10:14:30 +0900

    practicing oss

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-st.number_input('input number',0,10)
-st.title("practice")
-st.text_input('email')
-st.date_input('date')
-st.time_input('time(KOR)')
-st.text_area('description')
-st.file_uploader('upload photo')
-st.color_picker('Choose color')
+
+rand=np.random.normal(1,2,size =20)
+fig,ax = plt.subplots()
+ax.hist(rand,bins = 15)
+st.pyplot(fig)
